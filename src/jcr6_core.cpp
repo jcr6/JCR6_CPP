6faa8b2309a8a2e4d2bb6e3377c67adb66dd5ebb
Tricky1975 <facebook@tbbs.nl>
2019-02-20 15:36:04 +0100

    Concept core (For now reading only)

---
+++ src/jcr6_core.cpp
@@ -0,0 +1,58 @@
+#include "../headers/jcr6_core.hpp"
+
+
+// Just to use by JCR6, as I did not want to rely on "boost".
+// There is not guarantee all compilers support that, and this way I'm safe.
+static std::string Upper(std::string strToConvert)
+{
+    std::transform(strToConvert.begin(), strToConvert.end(), strToConvert.begin(), ::toupper);
+
+    return strToConvert;
+}
+
+
+namespace jcr6 {
+   // Prologue: Declarations
+   static std::string JAMJCR_Error;
+   const std::string &JCR_Error=tJCR_Error;
+
+   // Chapter 1: Reading
+   std::map <std::string,JT_Entry> &JT_Entry::Entries(){
+     // code comes later
+   }
+
+   JT_Entry *CreateEntry(std::string Name){
+     JAMJCR_Error = "Ok";
+     std::string id = Upper(Name);
+     // Is there a more elegant solution for this one?
+     JT_Entry cr;
+     EntryMap[id] = cr;
+     // Create pointer
+     JT_Entry *ret = &cr;
+     // Base config
+     ret->CFG_String["__ENTRY"] = name;
+     // return
+     return ret;
+   }
+
+
+   void RegisterDirDriver(JC_DirDriver Driver){
+     JAMJCR_Error = "Ok";
+     // code comes later
+   }
+
+   static map<std::string,JC_CompressDriver> CompDrivers;
+   void RegisterCompressDriver(JC_CompressDriver){
+     JAMJCR_Error = "Ok";
+     // code comes later
+   }
+
+   static map<std::string,JC_DirDriver> JC_DirDrivers;
+   void RegisterDirDriver(JC_DirDriver Driver){
+     JAMJCR_Error = "Ok";
+     // code comes later
+   }
+
+   // Chapter 2: Writing
+
+}
