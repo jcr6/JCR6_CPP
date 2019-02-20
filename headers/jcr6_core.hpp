d283f001bda7f8f4b6a2066522c07d53b671a7e8
Tricky1975 <facebook@tbbs.nl>
2019-02-20 15:54:13 +0100

    Function must be in header, only the macro won't work, eh? ;)

--- headers/jcr6_core.hpp
+++ headers/jcr6_core.hpp
@@ -42,6 +42,7 @@ namespace jcr6{
 
      // const std::string &JCR_Error; The clean way can't work, eh?
      #define JCR_Error jcr6::Get_JCR_Error_Message()
+     std::string() Get_JCR_Error_Message();
 
      class JT_Entry {
      private:
