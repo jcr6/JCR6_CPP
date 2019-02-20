af30f6793655012c76a669995b972d3772fe23a4
Tricky1975 <facebook@tbbs.nl>
2019-02-20 17:26:02 +0100

    Oh

--- src/jcr6_core.cpp
+++ src/jcr6_core.cpp
@@ -17,6 +17,7 @@
 //      misrepresented as being the original software.
 //   3. This notice may not be removed or altered from any source distribution.
 // EndLic
+
 #include "../headers/jcr6_core.hpp"
 
 
@@ -31,6 +32,7 @@ static std::string Upper(std::string strToConvert)
 
 
 namespace jcr6 {
+  
    // Prologue: Declarations
    static std::string JAMJCR_Error;
 
