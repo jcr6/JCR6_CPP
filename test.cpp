3d23e1a4fe1e6055cee261cefb31463eeb331e79
Tricky1975 <facebook@tbbs.nl>
2019-02-20 10:59:16 +0100

    No test binaries, please!

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "JCR6.hpp"
+
+int main(){
+	std::cout << "Testing utility for JCR6 in C++\nCreated by Jeroen P. Broks\nPlease use as you see fit!\n\n";
+	// Of course (this should go without saying), when you modify this file keep in mind you can "spook" up your repository folder
+	// causing git to malfunction when you try to update your clone from the actual repository.  Perhaps it's better to copy this file first
+	// before you make any modifications, in order to keep git happy :-P
+
+
+	return 0;
+}
