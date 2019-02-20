ea0b4eafbc9ebcd9661e261314f735682acc0884
Tricky1975 <facebook@tbbs.nl>
2019-02-20 10:48:24 +0100

    Main header (for now this only contains a comment and nothing more, later it will contain the actual calls to the header code. When it comes to the .h vs .hpp debate, I chose .hpp to clearly indicate that this is C++ code, however as my current C version of JCR6 is not exactly reaching my level of satisfaction, it's not unthinkable a "wrapper" for C will live in this very repository, and when that happens they all all contain just the .h extension.
    Since that is the matter right now, I wanted to avoid any possible confusion, and leave all options open).

---
+++ ProjectIcon/JCR6.hpp
@@ -0,0 +1,13 @@
+
+
+/*
+
+     This file contains the calls to the JCR6 core, and a few 
+     most expected to be needed drivers. In normal usage this
+     header should suffice. Of course, when you "know what
+     you are doing" you are free to come up with your own
+     setups... right?
+
+*/
+
+
