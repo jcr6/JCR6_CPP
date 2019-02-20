c581d7e1c17dccbf845ca017bf724043dc29972a
Tricky1975 <facebook@tbbs.nl>
2019-02-20 17:37:00 +0100

    Fix: forgotten * to denote pointers

--- src/jcr6_core.cpp
+++ src/jcr6_core.cpp
@@ -78,12 +78,12 @@ namespace jcr6 {
      DirDrivers[Driver.Name]=Driver;
    }
 
-   static int FakeStore(char * ibuffer,char obuffer int size){
+   static int FakeStore(char * ibuffer,char *obuffer, int size){
       for (int i=0;i<size;++i) obuffer[i]=ibuffer[i]; // Copying the buffer as a whole in stead of just copying the pointer is essential to make sure still will not get deleted when it shouldn't be!
       return size;
    }
 
-   static bool FakeRestore(char *ibuffer, char obuffer, int size1, int size2){
+   static bool FakeRestore(char *ibuffer, char *obuffer, int size1, int size2){
      FakeStore(ibuffer,obuffer,size2);
      return true;
    }
